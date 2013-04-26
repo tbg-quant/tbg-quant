c4d811269a1dbd983f0496924a47a895ae2669b5
tbg-quant <info@thebonnotgang.com>
2013-04-26 13:35:14 +0300

    Create IB_CancelingOrders_Esper.sh

---
+++ bin/IB_CancelingOrders_Esper.sh
@@ -0,0 +1,10 @@
+echo;
+echo WARNING !!
+echo;
+echo This example uses IB API and ESPER Library.
+echo Please download IB API and ESPER library and add the jars to the /lib folder.
+echo;
+echo TRADES WILL BE SENT TO THE BROKER AND EXECUTED, BE SURE TO LOGIN WITH DEMO OR PAPER ACCOUNT !!
+echo;
+read -p "Press any key...";
+java -cp ../tbg/*:../lib/*:../config/:../strategies-repo/*: examples.interactivebrokers.IB_CancelingOrders_Esper
