2afe7d642efa04633fdfca2fddcac9d2fd252556
Alberto Sfolcini <a.sfolcini@gmail.com>
2013-05-08 18:09:58 +0300

    Update YahooDataSynchronizer.bat

--- bin/YahooDataSynchronizer.bat
+++ bin/YahooDataSynchronizer.bat
@@ -1,2 +1,2 @@
-@java.exe -cp .;../tbg/*;../lib/*;../config/;../strategies-repo/*; utils.YahooDataSynchronizer
+@java.exe -cp .;../tbg/*;../lib/*;../config/;../strategies-repo/*; utils.YahooDataSynchronizer %1 %2 %3 %4 %5 %6 %7
 @pause;
