7272921e0c5b0665cd829b95f2a717a553e215b4
Alberto Sfolcini <a.sfolcini@gmail.com>
2013-05-08 18:09:36 +0300

    Update YahooDataSynchronizer.sh

--- bin/YahooDataSynchronizer.sh
+++ bin/YahooDataSynchronizer.sh
@@ -1,1 +1,1 @@
-java -cp ../tbg/*:../lib/*:../config/:../strategies-repo/*: utils.YahooDataSynchronizer
+java -cp ../tbg/*:../lib/*:../config/:../strategies-repo/*: utils.YahooDataSynchronizer $1 $2 $3 $4 $5 $6 $7
